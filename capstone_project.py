0125dc3c17cf70f3bc4ac433dd052357d7b56f09
Tedja Diah Rani Octavia <114978175+ranioc@users.noreply.github.com>
2024-03-16 15:50:19 +0700

    Update capstone_project.py

--- capstone_project.py
+++ capstone_project.py
@@ -71,25 +71,25 @@ with visualisasi:
 
     with col3:
         # Visualisasi: Distribusi Kamar Tidur
-        st.subheader('Distribusi Kamar Tidur')
-        bed_chart = alt.Chart(df).mark_bar().encode(
-            x=alt.X('Jumlah Kamar Tidur:N', title='Kamar Tidur'),
-            y='count():Q',
-            color='Kamar Tidur:N',
-            tooltip=['count():Q']
-        ).properties(width=600)
-        st.altair_chart(bed_chart, use_container_width=True)
+    st.subheader('Distribusi Kamar Tidur')
+    bed_chart = alt.Chart(df).mark_bar().encode(
+        x=alt.X('Kamar Tidur:N', title='Kamar Tidur'),
+        y='count():Q',
+        color='Kamar Tidur:N',
+        tooltip=['count():Q']
+    ).properties(width=600)
+    st.altair_chart(bed_chart, use_container_width=True)
 
     with col4:
         # Visualisasi: Distribusi Kamar Mandi
-        st.subheader('Distribusi Kamar Mandi')
-        bath_chart = alt.Chart(df).mark_bar().encode(
-            x=alt.X('Jumlah Kamar Mandi:N', title='Kamar Mandi'),
-            y='count():Q',
-            color='Kamar Mandi:N',
-            tooltip=['count():Q']
-        ).properties(width=600)
-        st.altair_chart(bath_chart, use_container_width=True)
+    st.subheader('Distribusi Kamar Mandi')
+    bath_chart = alt.Chart(df).mark_bar().encode(
+        x=alt.X('Kamar Mandi:N', title='Kamar Mandi'),
+        y='count():Q',
+        color='Kamar Mandi:N',
+        tooltip=['count():Q']
+    ).properties(width=600)
+    st.altair_chart(bath_chart, use_container_width=True)
 
     st.write('<div style="text-align: justify">Rumah dengan 2 Kamar Mandi dan 3 Kamar Tidur merupakan rumah yang paling banyak dijual. Hal ini dapat menandakan bahwa minat pasar yang banyak tertuju pada rumah dengan 3 Kamar TIdur dan 2 Kamar Mandi</div>', unsafe_allow_html=True)
     st.markdown("<br>", unsafe_allow_html=True)
